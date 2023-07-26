8a615b478940b3028bc971b982a1f7704782620b
Soroush-Pasandideh <86403507+Soroush-Pasandideh@users.noreply.github.com>
2023-07-26 15:11:19 +0330

    making TaskCreateView to be able to add new task.

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskListView, TaskDetailView
+from .views import TaskListView, TaskDetailView, TaskCreateView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetailView.as_view(), name='task'),
+    path('task-create/', TaskCreateView.as_view(), name='task-create'),
 ]
--- base/views.py
+++ base/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 from base.models import Task
 
@@ -15,3 +17,9 @@ class TaskDetailView(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
