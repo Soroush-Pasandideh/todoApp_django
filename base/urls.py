1963bbeabeb0162ccfe70d40941a29ae6fc502fa
Soroush-Pasandideh <86403507+Soroush-Pasandideh@users.noreply.github.com>
2023-07-27 02:18:59 +0330

    add user registration feature.

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView, CustomLoginView
+from .views import TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView, CustomLoginView, \
+    RegisterPageView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPageView.as_view(), name='register'),
 
     path('', TaskListView.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetailView.as_view(), name='task'),
