f1e8432316aefc8bb31629f67ce5e8c05936d956
Soroush-Pasandideh <86403507+Soroush-Pasandideh@users.noreply.github.com>
2023-07-26 15:20:02 +0330

    making TaskUpdateView to be able to update an existing task. also making some changes on task_list.html because of it.

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TaskListView, TaskDetailView, TaskCreateView
+from .views import TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetailView.as_view(), name='task'),
     path('task-create/', TaskCreateView.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdateView.as_view(), name='task-update'),
 ]
--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 
 from base.models import Task
@@ -20,6 +20,12 @@ class TaskDetailView(DetailView):
 
 
 class TaskCreateView(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdateView(UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
