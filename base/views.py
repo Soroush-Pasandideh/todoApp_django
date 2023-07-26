19976e1297cef2d7e34aff751e84cadd4511237d
Soroush-Pasandideh <86403507+Soroush-Pasandideh@users.noreply.github.com>
2023-07-26 14:06:45 +0330

    making TaskListView and TaskDetailView to get tasks list and a single task.

--- base/views.py
+++ base/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from base.models import Task
 
 
-class TaskList(ListView):
+class TaskListView(ListView):
     model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
