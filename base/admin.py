fceafe7f671ce7ab8c757e233ee50075e22238bd
Soroush-Pasandideh <86403507+Soroush-Pasandideh@users.noreply.github.com>
2023-07-29 15:19:12 +0330

    some changes on admin panel.

--- base/admin.py
+++ base/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
+
+from . import models
 from .models import Task
 
-admin.site.register(Task)
+
+# admin.site.register(Task)
+
+
+@admin.register(models.Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'username', 'complete']
+    list_per_page = 10
+    list_filter = ['user']
+    search_fields = ['title']
+
+    def username(self, task):
+        return task.user.username
